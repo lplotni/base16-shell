144418b0753a37538b39865d6468d887b0fdf5ee
Lukasz Plotnicki <lplotnicki@thoughtworks.com>
2017-03-28 09:27:16 +0200

    Using the original version of this script

--- profile_helper.sh
+++ profile_helper.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 script_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-for script in $script_dir/base16*.sh; do
-  script_name=$(basename $script .sh)
-  theme=${script_name#*-}
-  variation=${theme#*.}
-  theme=${theme%.*}
-  cat <<FUNC
-base16_${theme}_${variation}()
+
+. $script_dir/realpath/realpath.sh
+
+if [ -f ~/.base16_theme ]; then
+  script_name=$(basename $(realpath ~/.base16_theme) .sh)
+  echo "export BASE16_THEME=${script_name}"
+  echo ". ~/.base16_theme"
+fi
+cat <<'FUNC'
+_base16()
 {
-  . $script
-  ln -fs $script ~/.base16_theme 
-  export BASE16_THEME=base16-${theme}
-  export BASE16_VARIATION=$variation
-  echo $variation > ~/.base16_variation.rc
-  if type tmux_${variation} >/dev/null; then
-    tmux_${variation}
-  fi
-  [ -f ~/.vimrc_background ] && rm ~/.vimrc_background
-  cat <<VIMRC_BACKGROUND > ~/.vimrc_background
-set background=$variation
-colorscheme base16-$theme
-VIMRC_BACKGROUND
+  local script=$1
+  local theme=$2
+  [ -f $script ] && . $script
+  ln -fs $script ~/.base16_theme
+  export BASE16_THEME=${theme}
+  echo "colorscheme base16-$theme" > ~/.vimrc_background
 }
 FUNC
+for script in $script_dir/scripts/base16*.sh; do
+  script_name=${script##*/}
+  script_name=${script_name%.sh}
+  theme=${script_name#*-}
+  func_name="base16_${theme}"
+  echo "alias $func_name=\"_base16 \\\"$script\\\" $theme\""
 done;
-[ -f ~/.base16_theme ] && echo ". ~/.base16_theme"
-
